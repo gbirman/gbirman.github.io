c8d2a90c979c8fb3192da8d73752ec13a5c6e64a
Gabriel <25272206+gbirman@users.noreply.github.com>
2020-09-03 20:16:48 -0400

    added descriptions to portfolio modals

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,21 +7,6 @@
     */
     (function($) {
     "use strict"; // Start of use strict
-
-    let name = "Gabriel Birman"
-    let phone_number = "(609) 933-4082"
-    let email = "gabrielbirman1@gmail.com"
-
-    $(document).ready(() => {
-      //when the document is finished loading, replace everything
-      //between the <a ...> </a> tags with the value of splitText
-      // document.getElementById("emailLink").innerHTML = email;
-      document.getElementById("emailLink").setAttribute('href', "mailto:"+email);
-      document.getElementById("nameLink").innerHTML = name;
-      document.getElementById("aboutLink0").innerHTML = "Hello there! Wouldn't want to waste your time rambling on and on and on....so let's get started! My name's Gabriel, I'm a junior software developer who studied CS at Princeton (graduated in 2020). On this page you can find some of my coding projects, hobbies, and other miscellaneous information that is likely of no use to you. Enjoy!";
-      // document.getElementById("masthead-subheading").innerHTML = "Software Engineer by day, dreamer at night";
-    }
-    );
   
     // TODO: add projects
     // Chinese Language App
